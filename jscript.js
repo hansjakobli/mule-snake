ba2c9c3bc51d7ccf5156182e7bddb4b14ff052ca
Damian Scherrer <dscherrer@dscherr-ltml800.internal.salesforce.com>
2024-04-09 16:15:46 +0200

    Fewer game fields, bigger size

--- jscript.js
+++ jscript.js
@@ -1,6 +1,6 @@
 // GAME_PIXEL_COUNT is the pixels on horizontal or vertical axis of the game board (SQUARE).
 const AVAILABLE_FOOD = ['sap','boomi', 'wso2', 'axway', 'informatica', 'tibco', 'microsoft', 'workato', 'softwareag', 'apigee', 'jitterbit']
-const GAME_PIXEL_COUNT = 30;
+const GAME_PIXEL_COUNT = 20;
 const SQUARE_OF_GAME_PIXEL_COUNT = Math.pow(GAME_PIXEL_COUNT, 2);
 
 let totalFoodEaten = 0;
